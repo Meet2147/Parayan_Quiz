bf25abc7c962631feffb371fc99a2252e8b1cf8f
Meet Jethwa <meetjethwa3@gmail.com>
2023-09-05 21:35:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import random
+import pandas as pd
+import time
 
-# Define a list of sample questions and answers (you can replace these with your own data).
+# Sample questions and answers (you can replace these with your own data).
 questions = [
     {
         'question': 'What is the capital of France?',
@@ -16,19 +18,32 @@ questions = [
     # Add more questions here...
 ]
 
-# Initialize variables to track the user's score and current question.
+# Initialize user data for registration.
+user_data = []
+
+# Initialize variables for the quiz.
 user_score = 0
 current_question_index = 0
+quiz_in_progress = False
+start_time = None
 
-# Shuffle the questions for randomness.
-random.shuffle(questions)
+# Load leaderboard data from a CSV file (or database) if available.
+leaderboard_data = pd.read_csv('leaderboard.csv') if 'leaderboard.csv' in st.secrets else pd.DataFrame(columns=['User', 'Score'])
 
 # Streamlit app header.
 st.title('Quiz App')
 
+# Function to display the registration form.
+def registration():
+    st.subheader('User Registration')
+    username = st.text_input('Enter your name:')
+    if st.button('Register'):
+        user_data.append({'name': username, 'score': 0})
+        st.success(f'Welcome, {username}! You are now registered.')
+
 # Function to display a question and collect user's answer.
 def display_question():
-    global current_question_index, user_score
+    global current_question_index, user_score, quiz_in_progress, start_time
     if current_question_index < len(questions):
         question_data = questions[current_question_index]
         st.subheader(f'Question {current_question_index + 1}: {question_data["question"]}')
@@ -40,16 +55,45 @@ def display_question():
             else:
                 user_score += 1
             current_question_index += 1
+            start_time = time.time()  # Start the timer for the next question.
+    else:
+        quiz_in_progress = False
+        st.subheader('Quiz Finished!')
+        st.write(f'Your Score: {user_score}')
+
+# Function to update the leaderboard.
+def update_leaderboard():
+    global leaderboard_data
+    leaderboard_data = leaderboard_data.append({'User': user_data[-1]['name'], 'Score': user_score}, ignore_index=True)
+    leaderboard_data = leaderboard_data.sort_values(by='Score', ascending=False)
+    leaderboard_data.to_csv('leaderboard.csv', index=False)
+    st.subheader('Leaderboard')
+    st.dataframe(leaderboard_data)
 
 # Main quiz interface.
-if current_question_index < len(questions):
-    display_question()
+if not user_data:
+    registration()
+elif not quiz_in_progress:
+    if st.button('Start Quiz'):
+        current_question_index = 0
+        user_score = 0
+        random.shuffle(questions)
+        quiz_in_progress = True
+        start_time = time.time()  # Start the timer for the first question.
 else:
-    st.subheader('Quiz Finished!')
-    st.write(f'Your Score: {user_score}')
+    display_question()
+    elapsed_time = int(time.time() - start_time)
+    remaining_time = max(0, 10 - elapsed_time)
+    st.write(f'Time Remaining: {remaining_time} seconds')
+    if elapsed_time >= 10:
+        current_question_index += 1
+        start_time = time.time()  # Reset the timer for the next question.
 
-# Add a button to restart the quiz.
-if st.button('Restart Quiz'):
-    current_question_index = 0
-    user_score = 0
-    random.shuffle(questions)
+# Display the user's score and the leaderboard.
+if not user_data:
+    pass
+elif quiz_in_progress:
+    pass
+else:
+    st.write(f'Your Final Score: {user_score}')
+    update_leaderboard()
